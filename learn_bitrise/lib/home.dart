8e985a13fb01d94beef871541fa1dea35046e432
suljevicsemir <suljevic.semir@gmail.com>
2021-06-24 13:43:07 +0200

    Basic app setup

---
+++ learn_bitrise/lib/home.dart
@@ -0,0 +1,61 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrementCounter() {
+    setState(() {
+      counter--;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Value of the counter is:'),
+              Text(counter.toString())
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => incrementCounter(),
+            child: Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () => decrementCounter(),
+            child: Icon(Icons.remove),
+          )
+        ],
+      ),
+    );
+  }
+}
